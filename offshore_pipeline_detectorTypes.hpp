16e0e35169f4118c298c1c1a007d4d4ea66fe5c3
lmedagoda <lashika.medagoda@dfki.de>
2017-03-06 17:26:01 +0100

    intial commit for workshop

---
+++ offshore_pipeline_detectorTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef offshore_pipeline_detector_TYPES_HPP
+#define offshore_pipeline_detector_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace offshore_pipeline_detector {
+}
+
+#endif
+
